3659688052846cbe3255b9b0806b3a09de27591c
Pravin Singh Rana <dev.psrana@gmail.com>
2025-06-10 10:12:33 +0530

    removed the dependancy from the rest countries api

--- utils/Telr-helper.php
+++ utils/Telr-helper.php
@@ -3,6 +3,23 @@ if (!defined('ABSPATH'))
     exit;
 class Telr_helper
 {
+    public $countries;
+    public function __construct()
+    {
+        if (file_exists(TELR_PLUGIN_DIR . '/data/countries.json')) {
+            $countries_data = file_get_contents(TELR_PLUGIN_DIR . '/data/countries.json');
+            $data = json_decode($countries_data, true);
+            if (json_last_error() === JSON_ERROR_NONE) {
+                $this->countries = $data;
+            } else {
+                error_log('Invalid JSON in countries.json');
+                $this->countries = [];
+            }
+        }
+
+
+
+    }
     public function generate_cart_id()
     {
         $cart_id = wp_generate_uuid4();
@@ -10,68 +27,47 @@ class Telr_helper
     }
     public function get_country_from_code($code)
     {
-        $code = strtoupper(sanitize_text_field($code));
-        $url = "https://restcountries.com/v3.1/alpha/{$code}";
+        $code = sanitize_text_field($code);
+        $code = strtoupper($code);
 
-        $response = wp_remote_get($url);
-
-        if (is_wp_error($response)) {
-            return 'Country not found';
+        foreach ($this->countries as $country) {
+            if ($country['code'] === $code) {
+                return $country['name'];
+            }
         }
 
-        $body = wp_remote_retrieve_body($response);
-        $data = json_decode($body, true);
-
-        if (!empty($data) && isset($data[0]['name']['common'])) {
-            return esc_html($data[0]['name']['common']);
-        }
-
-        return 'Country not found';
+        return null;
     }
     public function get_country_code_from_name($name)
     {
         $name = sanitize_text_field($name);
         $name = strtolower($name);
-        $url = "https://restcountries.com/v3.1/name/{$name}";
-
-        $response = wp_remote_get($url);
-
-        if (is_wp_error($response)) {
-            return 'Country code not found';
+        $name = ucwords($name);
+        foreach ($this->countries as $country) {
+            if ($country['name'] === $name) {
+                return $country['code'];
+            }
         }
 
-        $body = wp_remote_retrieve_body($response);
-        $data = json_decode($body, true);
+        return null;
 
-        if (!empty($data) && isset($data[0]['cca2'])) {
-            return esc_html($data[0]['cca2']);
-        }
 
-        return 'Country code not found';
     }
     public function get_all_countries()
     {
-        $url = "https://restcountries.com/v3.1/independent?status=true";
 
-        $response = wp_remote_get($url);
-
-        if (is_wp_error($response)) {
-            return [];
-        }
-
-        $body = wp_remote_retrieve_body($response);
-        $data = json_decode($body, true);
+        $data = $this->countries;
         $exclude = ['IR', 'CU', 'KP', 'SD', 'SS', 'UA', 'SY', 'RU', 'MM', 'YE']; // country codes to exclude
 
         $countries = [];
         foreach ($data as $country) {
             if (
-                isset($country['name']['common'], $country['cca2']) &&
-                !in_array(strtoupper($country['cca2']), $exclude, true)
+                isset($country['name'], $country['code']) &&
+                !in_array(strtoupper($country['code']), $exclude, true)
             ) {
                 $countries[] = [
-                    'name' => esc_html($country['name']['common']),
-                    'code' => esc_html($country['cca2']),
+                    'name' => esc_html($country['name']),
+                    'code' => esc_html($country['code']),
                 ];
             }
         }
